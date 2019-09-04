071f90972d78fb0c0289418a594deb9db16d6a94
David Loder <davidloder92@gmail.com>
2019-09-04 00:06:34 -0400

    elementByClassName-update1

--- app.js
+++ app.js
@@ -2,6 +2,18 @@ const myHeading = document.getElementById('myHeading');
 const myButton = document.getElementById('myButton');
 const myTextInput = document.getElementById('myTextInput');
 
+<<<<<<< Updated upstream
 myButton.addEventListener('click', () => {
   myHeading.style.color = myTextInput.value;
                            });
+=======
+for (let i = 0; i < myList.length; i += 1) {
+    myList[i].style.color = 'purple';
+}
+
+const errorNotPurple = document.getElementsByClassName('error-not-purple');
+
+for (let i = 0; i < errorNotPurple.length; i += 1) {
+    errorNotPurple[i].style.color = 'red';
+}
+>>>>>>> Stashed changes
