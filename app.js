b36719c68d46e7bffd2c76c7794b90fdc1ea975a
David Loder <davidloder92@gmail.com>
2019-09-04 00:49:22 -0400

    elementByClassName-update1

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
-const myHeading = document.getElementById('myHeading');
-const myButton = document.getElementById('myButton');
-const myTextInput = document.getElementById('myTextInput');
+const myList = document.getElementsByTagName('li');
 
-<<<<<<< Updated upstream
-myButton.addEventListener('click', () => {
-  myHeading.style.color = myTextInput.value;
-                           });
-=======
+
 for (let i = 0; i < myList.length; i += 1) {
     myList[i].style.color = 'purple';
 }
 
-const errorNotPurple = document.getElementsByClassName('error-not-purple');
+const errorNotPurple = document.querySelectorAll('.error-not-purple');
 
 for (let i = 0; i < errorNotPurple.length; i += 1) {
     errorNotPurple[i].style.color = 'red';
 }
->>>>>>> Stashed changes
+
+const evens = document.querySelectorAll('li:nth-child(odd)');
+
+for (let i = 0; i < evens.length; i += 1) {
+    evens[i].style.backgroundColor = 'lightgray';
+}
